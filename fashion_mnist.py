52e5673cf987ce1d3290b0c48dabf9b2d4debcbd
Tim Burtell <tburtell@emich.edu>
2020-06-21 14:16:13 -0400

    Initial commit of fashion_mnist.py, taken from Tensorflow Tutorial and cut out the fat

---
+++ fashion_mnist.py
@@ -0,0 +1,58 @@
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+
+model.compile(optimizer=tf.keras.optimizers.SGD(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+
+# Grab an image from the test dataset.
+img = test_images[1]
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+
+predictions_single = probability_model.predict(img)
+
+print(predictions_single)
+
+
+np.argmax(predictions_single[0])
